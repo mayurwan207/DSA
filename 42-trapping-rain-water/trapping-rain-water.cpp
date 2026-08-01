2e34da54ceaca85d9e67a7068d7c4b19661381f3
mayurwan207 <mayureshwankhade968@gmail.com>
2026-08-01 18:35:58 +0000

    Time: 0 ms (100.00%) | Memory: 26.1 MB (45.02%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // Left side is the bottleneck
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                // Right side is the bottleneck
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+};
