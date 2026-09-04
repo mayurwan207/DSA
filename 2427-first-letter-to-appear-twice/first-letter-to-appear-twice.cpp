235c061eb64607412657faef8504222a23ecc58d
mayurwan207 <mayureshwankhade968@gmail.com>
2026-09-05 00:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (43.40%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        
+        unordered_set<char> set;
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            if(set.find(s[i]) != set.end()) {
+                return s[i];
+            }
+            
+            set.insert(s[i]);
+        }
+        
+        return ' ';
+    }
+};
