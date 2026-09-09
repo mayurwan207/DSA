b39fb6d837481f7a69fd6d2d5cefdbbd10f6afa4
mayurwan207 <mayureshwankhade968@gmail.com>
2026-09-09 16:39:09 +0530

    Time: 13 ms (82.10%) | Memory: 28.4 MB (64.51%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string stack;
+        for(char c:s){
+            if( c == '*'){
+                stack.pop_back();
+            }
+            else{
+                stack.push_back(c);
+            }
+        }
+        return stack;
+        
+    }
+};
